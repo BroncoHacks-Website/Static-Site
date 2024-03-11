85eb3089edacdf492a494fbe9c262e15d11abb4f
chillwafflez <ju5t1n.nguy3n284@gmail.com>
2024-03-10 18:43:04 -0700

    setup project for Task1

--- Task1/src/App.jsx
+++ Task1/src/App.jsx
@@ -1,13 +1,16 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-// import './App.css'
+import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <div> Import your Mini Profile Component below!</div>
+    <div>
+      <div class="header-container">
+        <h3> Place your Mini Profile Component below!</h3>
+      </div>
+      {/*   Insert your Profile Component here   */}
+    </div>
   )
 }
 
