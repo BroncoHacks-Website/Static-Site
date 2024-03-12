a4e34362513dd67674ae3cc42d0bfc52c11a0273
xdkaine <55013938+xdkaine@users.noreply.github.com>
2024-03-12 15:57:20 -0700

    created the profile component designed the two different profile component for other future profiles imported the profile component into app.jsx

--- Task1/src/App.jsx
+++ Task1/src/App.jsx
@@ -2,14 +2,15 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Profile from './components/Profile.jsx';
 
 function App() {
   return (
     <div>
-      <div class="header-container">
+      <div className="header-container">
         <h3> Place your Mini Profile Component below!</h3>
       </div>
-      {/*   Insert your Profile Component here   */}
+      {<Profile></Profile>}
     </div>
   )
 }
---
+++ Task1/src/components/Profile.jsx
@@ -0,0 +1,20 @@
+function Profile() {
+    return (
+    <div>
+        <div className="profilegrid">
+            <div className="profile">
+            <img className="profilepicture" src="https://media.licdn.com/dms/image/D5603AQFI7ZBRCOheqQ/profile-displayphoto-shrink_200_200/0/1698549537540?e=1715817600&v=beta&t=ojbaGRCop6sU6VzK6VmFyc9n0dPMdnSQPrvw2E-iL_I"></img>
+                <div className="profilebody">
+                    <name>Tommy Phao</name>
+                    <major>Business Administration - Computer Information Systems </major>
+                    <etc>Second Year </etc>
+                    <etc>https://github.com/xdkaine</etc>
+                </div>
+            </div>
+        </div>
+    </div>
+    )
+  }
+  
+  
+  export default Profile
