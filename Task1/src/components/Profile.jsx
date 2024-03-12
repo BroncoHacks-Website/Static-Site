6deda2ff561384c973f2e87b4cb9d485803b917a
Rebecca Smith <2145912+Rebeccals@users.noreply.github.com>
2024-03-11 20:37:33 -0700

    Added profile component
    Added a profile component to the task1 branch (hopefully)

---
+++ Task1/src/components/Profile.jsx
@@ -0,0 +1,18 @@
+
+function Profile() {
+    return (
+      <section class="main-container">
+        <article class="profile-container">
+            <picture class="img-container" role="img" aria-label="{Perplexed Woman}" alt="Perplexed Woman"></picture>
+            <content class="text-box">
+                <h3>Rebecca Smith</h3>
+                <p>Junior</p>
+                <p>Computer Science</p>
+                <a href="https://github.com/Rebeccals">Github profile</a>
+            </content>
+        </article>
+      </section>
+    )
+  }
+  
+  export default Profile
