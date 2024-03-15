891c808f6f24b91945befc6048d5972fadf4c0fe
tommy phao <thomasphao04@gmail.com>
2024-03-14 18:06:26 -0700

    added basic navbar component

--- Task1/src/App.jsx
+++ Task1/src/App.jsx
@@ -2,11 +2,17 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import Profile from './components/Profile.jsx'; 
+<<<<<<< Updated upstream
+=======
+import Profile from './components/Profile.jsx';
+import Navbar from './components/Navbar.jsx';
+
+>>>>>>> Stashed changes
 
 function App() {
   return (
     <div>
+      {<Navbar></Navbar>}
       <div className="header-container">
         <h3> Place your Mini Profile Component below!</h3>
       </div>
---
+++ Task1/src/components/Navbar.jsx
@@ -0,0 +1,18 @@
+function Navbar() {
+    return (
+        <div>
+            <div className="navigationbar">
+                <div className="logo">
+                    insert logo
+                </div>
+                <div className="buttons">
+                    <a href="/home">Home</a>
+                    <a href="/about">about</a>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+
+export default Navbar
