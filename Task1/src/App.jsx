31492f2293f926ff290336fed914cab72dce7140
xdkaine <55013938+xdkaine@users.noreply.github.com>
2024-03-14 18:03:48 -0700

    created navbar branch with profiles

--- Task1/src/App.jsx
+++ Task1/src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import Profile from './components/Profile.jsx';
+import Profile from './components/Profile.jsx'; 
 
 function App() {
   return (
