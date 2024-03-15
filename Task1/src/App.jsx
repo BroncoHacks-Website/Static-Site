1e1b0c874ed2e9cad6075698ddbe08084b2e1681
rane20 <145800950+rane20@users.noreply.github.com>
2024-03-15 16:18:30 -0700

    Added profile component(Hopefully)

--- Task1/src/App.jsx
+++ Task1/src/App.jsx
@@ -2,6 +2,9 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import React from 'react';
+import Profile from './components/Profile';
+
 
 function App() {
   return (
@@ -9,7 +12,7 @@ function App() {
       <div class="header-container">
         <h3> Place your Mini Profile Component below!</h3>
       </div>
-      {/*   Insert your Profile Component here   */}
+      <Profile />
     </div>
   )
 }
