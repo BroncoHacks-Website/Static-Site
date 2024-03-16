7e9f02c8aea4c0e0de2c025341c90f4caea7db6a
rane20 <145800950+rane20@users.noreply.github.com>
2024-03-15 21:59:48 -0700

    Attempt 2 with Profile

--- Task1/src/App.jsx
+++ Task1/src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Profile from './Components/Profile'
 
 function App() {
   return (
@@ -9,7 +10,7 @@ function App() {
       <div class="header-container">
         <h3> Place your Mini Profile Component below!</h3>
       </div>
-      {/*   Insert your Profile Component here   */}
+      <Profile/> {/*   Insert your Profile Component here   */}
     </div>
   )
 }
