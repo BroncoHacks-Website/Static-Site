383283b58ecaba480980c43072b371f182535233
rane20 <145800950+rane20@users.noreply.github.com>
2024-03-16 12:54:16 -0700

    Added more into profile component and did nav bar(i think)

--- Task1/src/App.jsx
+++ Task1/src/App.jsx
@@ -3,11 +3,13 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Profile from './Components/Profile'
+import NavigationBar from './Components/NavigationBar'
 
 
 function App() {
   return (
     <div>
+      <NavigationBar/>
       <div class="header-container">
         <h3> Place your Mini Profile Component below!</h3>
       </div>
