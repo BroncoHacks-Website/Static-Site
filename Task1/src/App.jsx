55a2ba4cd6363be77d63f43171c14cccea98616b
rane20 <145800950+rane20@users.noreply.github.com>
2024-03-15 22:52:17 -0700

    Fixed git hub link

--- Task1/src/App.jsx
+++ Task1/src/App.jsx
@@ -2,27 +2,16 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-<<<<<<< HEAD
 import Profile from './Components/Profile'
-=======
-import React from 'react';
-import Profile from './components/Profile';
-import NavigationBar from './Components/NavigationBar';
 
->>>>>>> ed22f89e8f56553255292b296407905200f33613
 
 function App() {
   return (
     <div>
-      <NavigationBar/>
       <div class="header-container">
         <h3> Place your Mini Profile Component below!</h3>
       </div>
-<<<<<<< HEAD
       <Profile/> {/*   Insert your Profile Component here   */}
-=======
-      <Profile />
->>>>>>> ed22f89e8f56553255292b296407905200f33613
     </div>
   )
 }
