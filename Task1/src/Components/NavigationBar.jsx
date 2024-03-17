142b93a82d9d9346abae6a7d9a3e7541edfdd3a9
rane20 <145800950+rane20@users.noreply.github.com>
2024-03-16 21:52:18 -0700

    did it work?

---
+++ Task1/src/Components/NavigationBar.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function NavigationBar(){
+    return (
+        <div className="navigation-bar">
+            <a href="/home">Home</a>
+            <a href="/aboutus">About Us</a>
+        </div>
+    );
+}
+
+export default NavigationBar;
---
+++ Task1/src/Components/Profile.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+function Profile(){
+    return(
+        <div>
+            <h2>My Profile</h2>
+            <img src="/IMG_4382.jpg" alt= "" width="250" height="200"/>
+            <p>Name: Rane Dy</p>
+            <p>Major: Computer Science</p>
+            <p>Academic year: Junior</p>
+            <p>
+                <a href="https://github.com/rane20">My Github</a>
+            </p>
+        </div>
+    )
+}
+
+export default Profile;
