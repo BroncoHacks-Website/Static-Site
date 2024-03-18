8a5eb350c28186149c14c7d6fbde33befa84365d
Maddie Isaacs <114186981+mmisaacs@users.noreply.github.com>
2024-03-17 20:24:50 -0700

    Create navbar.jsx

---
+++ Task1/src/components/navbar.jsx
@@ -0,0 +1,12 @@
+function navbar() {
+  return(
+    <div class="container">
+      <div class="topnav">
+        <li><a href="aboutus.jsx">About Us</a></li>
+        <li><a href="apply.jsx">APPLY</a></li>
+      </div>
+    </div>
+    );
+}
+
+export default navbar;
