70a7fa175d9c1a2bc68546d85ae854188a8c8b33
Maddie Isaacs <misaacs@cpp.edu>
2024-03-24 19:38:49 -0700

    upload icons and logos

--- Static-Site/src/Components/Footer.jsx
+++ Static-Site/src/Components/Footer.jsx
@@ -1,9 +1,30 @@
 function Footer() {
     
     return (
-      <>
-        <div>Footer Component</div>
-      </>
+      <footer>
+        <div class="container">
+          <div class="footer-content">
+            <img src="../src/Assets/BroncoHacks_Logo.png" alt="Bronco Hacks Logo"></img>
+          </div>
+          <div class="footer-content">
+            <div class="text">Made With ♥ by the Bronco Hacks team</div>
+            <div class="links">
+              <a href="https://www.instagram.com/cppbroncohacks/" target="_blank" rel="noopener noreferrer">
+                <img src="../src/Assets/Footer/ig icon.png" alt="Bronco Hacks Instagram"></img>
+              </a>
+              <a href="https://discord.gg/pEXRD5Zyvd" target="_blank" rel="noopener noreferrer">
+                <img src="../src/Assets/Footer/discord icon.png" alt="Bronco Hacks Discord"></img>
+              </a>
+              <a href="mailto:uf.cppbroncohacks@gmail.com" target="_blank" rel="noopener noreferrer">
+                <img src="../src/Assets/Footer/mail icon.png" alt="Email Bronco Hacks"></img>
+              </a>
+            </div>
+          </div>
+        </div>
+        <div class="bottom-bar">
+          <p>&copy; 2024 BroncoHacks. All rights reserved.</p>
+        </div>
+      </footer>
     )
   }
   
