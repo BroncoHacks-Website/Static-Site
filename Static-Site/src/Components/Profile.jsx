ee832c19d04f383a54cebb61690c0b73c0be7b04
arminerika <arminerika@gmail.com>
2024-04-07 19:55:14 -0700

    filter and sort on button click instead of with index + test mockup css

--- Static-Site/src/Components/Profile.jsx
+++ Static-Site/src/Components/Profile.jsx
@@ -1,21 +1,26 @@
-import "../styles/Profile.css"; /* temporary just so it visually looks fine */
+// Profile.jsx
 
-function Profile({ person }) {
+import '/src/Styles/Profile.css'; // temporary just so it visually looks fine
+
+function Profile({person}) {
+
   return (
     <div className="profile">
       <img
         className="profilepicture"
         src={person.picture}
-        alt={"Picture of" + person.name}
+        alt={"Picture of " + person.name}
       />
       <div className="profilebody">
-        <name>{person.name}</name>
-        <role>{person.role}</role>
+        <div className="name">{person.name}</div>
+        <div className="role">
+          {Array.isArray(person.role) ? person.role.join(', ') : person.role}</div>
       </div>
       <a href={person.linkedin}>
         <img
           className="linkedinpicture"
-          src="../src/Assets/Profile/icon/linkedin-small.png"
+          src="/src/Assets/Profile/icon/linkedin-small.png"
+          alt="LinkedIn"
         />
       </a>
     </div>
