fc979fabb35e0e91e7ecbc827fc88d4e0b024e75
Rebecca Smith <2145912+Rebeccals@users.noreply.github.com>
2024-07-27 12:11:57 -0700

    Hopefully, fixed issues with icons not showing up.

--- Static-Site/src/Components/AboutAssociationBox.jsx
+++ Static-Site/src/Components/AboutAssociationBox.jsx
@@ -7,23 +7,15 @@ function AboutAssociationBox() {
         <div className = "clubs-container">
             <h2>In Association With</h2>
             <ul>
-                <li><a href="https://www.instagram.com/cppcss/" target="_blank" rel="noopener noreferrer"><img src="https://ugc.production.linktr.ee/12da207a-6379-4d6a-b8ff-1b8fbbca1ad4_CFD8FBB7-FFEE-4DA9-A2FE-3C667A69CAE3.png?io=true&size=avatar-v3_0"/></a></li>
-
-                <li><a href="https://www.instagram.com/gdsccpp/" target="_blank" rel="noopener noreferrer"><img src="https://ugc.production.linktr.ee/4p5hpFASIaaOcaWE6AdQ_tBDmCVo0020fsjeY?io=true&size=avatar-v3_0"/></a></li>
-
-                <li><a href="https://www.instagram.com/cppgamedev/" target="_blank" rel="noopener noreferrer"><img src="https://yt3.googleusercontent.com/ytc/AIdro_mX4aIKbf6Z1RWWcYliFYLyR3zqGr6-uBpxyz7tz65jX-M=s176-c-k-c0x00ffffff-no-rj"/></a></li>
+                <li><a href="https://www.instagram.com/cppcss/" target="_blank" rel="noopener noreferrer"><img src="../src/Assets/About/imgs/cppcss.jpg"/></a></li>
+                <li><a href="https://www.instagram.com/gdsccpp/" target="_blank" rel="noopener noreferrer"><img src="../src/Assets/About/imgs/gdsccpp.jpg"/></a></li>
+                <li><a href="https://www.instagram.com/calpolyscript/" target="_blank" rel="noopener noreferrer"><img src="../src/Assets/About/imgs/calpolyscrip.jpg"/></a></li>
             </ul>
             <ul>
-                <li><a href="https://www.instagram.com/calpolyfast/" target="_blank" rel="noopener noreferrer"><img src="https://cdn.campsite.bio/eyJidWNrZXQiOiJjYW1wc2l0ZS1iaW8tc3RvcmFnZSIsImtleSI6IkNhbFBvbHlGQVNULzk2ZmQ2OGFhLWM4ODEtNGZmYi05YjhjLWEwODliZWNiOWQyMC5wbmciLCJlZGl0cyI6eyJyZXNpemUiOnsid2lkdGgiOjIwMH19fQ=="/></a></li>
-
-                <li><a href="https://www.instagram.com/cppmissa/" target="_blank" rel="noopener noreferrer"><img src="https://ugc.production.linktr.ee/8csxebyRR9O3Fi4MdE0K_1BZQDBzEFM9N4Ig3?io=true&size=avatar-v3_0"/></a></li>
-
-                <li><a href="https://www.instagram.com/cppshecodes/" target="_blank" rel="noopener noreferrer"><img src="https://ugc.production.linktr.ee/yNbRfvBfTtyUfJyBW4Gp_rIXkg0O78wgQQI3O?io=true&size=avatar-v3_0"/></a></li>
-
-                {/* <li><a href="https://www.instagram.com/calpolyscript/" target="_blank" rel="noopener noreferrer"><img src="../src/Assets/Profile/icon/script.png"/></a></li> */}
-                {/* remove line below once line 22 works for sure */}
-
-                <li><a href="https://www.instagram.com/calpolyscript/" target="_blank" rel="noopener noreferrer"><img src="https://cdn.iconscout.com/icon/free/png-256/free-node-js-1174925.png?f=webp"/></a></li>
+                <li><a href="https://www.instagram.com/cppmissa/" target="_blank" rel="noopener noreferrer"><img src="../src/Assets/About/imgs/cppmissa.jpg"/></a></li>
+                <li><a href="https://www.instagram.com/cppshecodes/" target="_blank" rel="noopener noreferrer"><img src="../src/Assets/About/imgs/cppshecodes.jpg"/></a></li>
+                <li><a href="https://www.instagram.com/cppgamedev/" target="_blank" rel="noopener noreferrer"><img src="../src/Assets/About/imgs/cppgamedev.jpg"/></a></li>
+                <li><a href="https://www.instagram.com/calpolyfast/" target="_blank" rel="noopener noreferrer"><img src="../src/Assets/About/imgs/calpolyfast.jpg"/></a></li>
             </ul>
         </div>
       </>
