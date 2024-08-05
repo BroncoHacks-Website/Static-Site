9b7d59f191f252b5c601a233c9eed8a6672e2110
xdkaine <55013938+xdkaine@users.noreply.github.com>
2024-08-04 18:01:29 -0700

    Removed winners implemenation on home page

--- Static-Site/src/Pages/Home.jsx
+++ Static-Site/src/Pages/Home.jsx
@@ -2,7 +2,6 @@ import Slideshow from '/src/Components/Slideshow.jsx';
 import { slidesData } from "../Assets/Profile/slidesData";
 import DescriptionBox from '../Components/DescriptionBox';
 import Countdown from '../Components/Countdown';
-import Winners from '../Components/WinnerCard';
 import '/src/styles/Home.css'
 
 function Home() {
@@ -19,7 +18,6 @@ function Home() {
             </div>
           </div>
           <Countdown deadline="October, 31, 2024" />
-          <Winners/>
         </div>
       </>
     )
